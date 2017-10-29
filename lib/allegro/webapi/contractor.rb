baa1039406c9fd0a1d794279b9dbc9132773f5c1
Bartłomiej Matlęga <bartlomiej.matlega95@gmail.com>
2017-10-29 21:41:18 +0100

    Adds feedback method

--- lib/allegro/webapi/contractor.rb
+++ lib/allegro/webapi/contractor.rb
@@ -12,6 +12,25 @@ module Allegro
             .call(:do_get_post_buy_forms_data_for_sellers, message: message)
             .body[:do_get_post_buy_forms_data_for_sellers_response]
       end
+
+      def do_add_package_info_to_post_buy_form(transactions_id, operator_id, package_id)
+          message = {
+            session_id: @api_client.session_handle,
+            transaction_id: transactions_id,
+            package_info: {
+              item: [
+                {
+                  operator_id: operator_id,
+                  package_id: package_id
+                }
+              ]
+            }
+          }
+
+          @api_client
+            .call(:do_add_package_info_to_post_buy_form, message: message)
+            .body[:do_add_package_info_to_post_buy_form_response]
+      end
     end
   end
 end
