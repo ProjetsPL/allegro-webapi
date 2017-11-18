0c4f2f452b1a63e6afcbc8cbf99929a2362e86fc
Bartłomiej Matlęga <bartlomiej.matlega95@gmail.com>
2017-11-18 16:15:45 +0100

    Added listing'

---
+++ lib/allegro/webapi/listing.rb
@@ -0,0 +1,22 @@
+module Allegro
+  module WebApi
+    module Listing
+      def do_get_items_list(filter_options = [], sort_options = [], result_size = nil, result_offset = nil, result_scope = nil)
+        message = {
+          webapi_key: @api_client.webapi_key,
+          country_id: @api_client.country_code,
+          filter_options: filter_options,
+          sort_options: sort_options,
+          result_size: result_size,
+          result_offset: result_offset,
+          result_scope: result_scope
+        }
+
+        @api_client
+          .call(:do_get_items_list, message: message)
+          .body[:do_get_items_list_response]
+      end
+
+    end
+  end
+end
